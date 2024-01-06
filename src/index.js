0a0d20f57a5a6fd419e4415b52382e2f020a4189
1Dmar <121735575+1Dmar@users.noreply.github.com>
2024-01-06 12:29:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ configureServer(app);
 
 // Railway automatically injects this at runtime, and you don't even
 // have to explicitly add it in the dashboard.
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7402;
 
 app.listen(PORT, () => {
   console.log(`Server is up on PORT ${PORT}`);
